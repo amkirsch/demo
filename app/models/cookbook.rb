2d7656286c128859c09433f1aec60eb237d73b73
Andrew Kirschke <amkirsch@us.ibm.com>
2016-06-27 21:47:10 -0400

    changes

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -1,6 +1,9 @@
 class Cookbook < ActiveRecord::Base
   has_many :recipes
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
   scope :oldest_first, lambda { order("cookbooks.created_at ASC") }
   scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
 end
--- db/migrate/20160616131644_create_resources.rb
+++ db/migrate/20160616131644_create_resources.rb
@@ -2,10 +2,10 @@ class CreateResources < ActiveRecord::Migration
   def change
     create_table :resources do |t|
       t.integer "recipe_id"
-      t.string :name
-      t.string :resource_type
-      t.text :properties
-      t.text :actions
+      t.string :name, null: false
+      t.string :resource_type, null: false
+      t.text :properties, null: false
+      t.text :actions, null: false
 
       t.timestamps null: false
     end
