13b6cc2ad7d29ee926508e5cb3785d12edf29de3
Andrew Kirschke <andrew.kirschke@gmx.com>
2016-07-05 08:14:37 -0400

    Nearly finished with first iteration of drag and drop. GUI worked as intended, needs more testing and position attribute added to resource object.

--- app/assets/javascripts/designer.js
+++ app/assets/javascripts/designer.js
@@ -40,8 +40,36 @@ function prepareDesignerFunctions() {
       e.stopPropagation(); // stops the browser from redirecting.
     }
     // Don't do anything if dropping the same resource we're dragging.
-    if (dragSrcEl != this) {
+
+    var recipes = document.getElementsByClassName('recipe-li');
+    [].forEach.call(recipes, function(recipe) {
+      if (e.currentTarget === recipe) {
+        console.log('found recipe: ' + recipe.parentNode.getAttribute('recipe-id'));
+        console.log(dragSrcEl);
+        recipe.parentNode.appendChild(dragSrcEl)
+        return false
+      }
+    });
+    // var resources = document.querySelectorAll('.resource');
+    // [].forEach.call(resources, function(resource, i) {
+    //   console.log('comparing resource: ' + resource + ' to ' + e.currentTarget);
+    //   if (e.currentTarget === resource && dragSrcEl != e.currentTarget) {
+    //     // Set the source resource's HTML to the HTML of the resource we dropped on.
+    //     srcId = dragSrcEl.getAttribute('resource-id');
+    //     dragSrcEl.setAttribute('resource-id', this.getAttribute('resource-id'));
+    //     this.setAttribute('resource-id', srcId);
+    //
+    //     dragSrcEl.innerHTML = this.innerHTML;
+    //     this.innerHTML = e.dataTransfer.getData('text/html');
+    //   }
+    // });
+
+    if (dragSrcEl != e.currentTarget && e.currentTarget.hasAttribute('resource-id')) {
       // Set the source resource's HTML to the HTML of the resource we dropped on.
+      srcId = dragSrcEl.getAttribute('resource-id');
+      dragSrcEl.setAttribute('resource-id', this.getAttribute('resource-id'));
+      this.setAttribute('resource-id', srcId);
+
       dragSrcEl.innerHTML = this.innerHTML;
       this.innerHTML = e.dataTransfer.getData('text/html');
     }
@@ -51,23 +79,18 @@ function prepareDesignerFunctions() {
 function saveItem(item) {
   var request = new XMLHttpRequest();
   var token = document.querySelector('meta[name="csrf-token"]').content;
-  console.log(token);
   request.open('PUT', encodeURI('designer/sort?' + item));
   request.setRequestHeader('X-CSRF-Token', token);
   request.send();
 }
-function gatherPositions() {
+function updatePositions() {
   var recipes = document.querySelectorAll('.recipes div');
-  console.log('recipes = ' + recipes);
   [].forEach.call(recipes, function(recipe) {
     var resources = recipe.querySelectorAll('.resource');
-    console.log('resources = ' + resources);
     [].forEach.call(resources, function(resource, i) {
-      console.log('level2');
       var position = i + 1;
       var item = 'sort[recipe_id]=' + recipe.getAttribute('recipe-id') + '&sort[resource_id]=' + resource.getAttribute('resource-id') + '&sort[resource_position]=' + position;
       saveItem(item);
-      console.log(item);
     });
   });
 }
@@ -77,9 +100,8 @@ function gatherPositions() {
       resource.classList.remove('over');
       resource.style.opacity = '1.0';  //this / e.target is the source node.
     });
-    console.log('before');
-    gatherPositions();
-    console.log('after');
+    updatePositions();
+    dragSrcEl = null;
   }
 
   // var resources = document.querySelectorAll('#resources .resource');
@@ -91,6 +113,14 @@ function gatherPositions() {
     resource.addEventListener('dragleave', handleDragLeave, false);
     resource.addEventListener('drop', handleDrop, false);
     resource.addEventListener('dragend', handleDragEnd, false);
+  });
+
+  var recipes = document.getElementsByClassName('recipe');
+  [].forEach.call(recipes, function(recipe) {
+    recipe.parentNode.addEventListener('drop', handleDrop, false);
+    recipe.parentNode.addEventListener('dragover', handleDragOver, false);
+    recipe.parentNode.addEventListener('dragleave', handleDragLeave, false);
+    recipe.parentNode.addEventListener('dragend', handleDragEnd, false);
   });
 
 }
